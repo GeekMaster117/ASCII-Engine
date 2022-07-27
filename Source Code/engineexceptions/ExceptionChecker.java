b6f62d197636f38d37b92ef527e1c4ee673fc586
GeekMaster117 <saivenkat0306@gmail.com>
2022-07-27 11:53:40 +0530

    Code

--- Source Code/engineexceptions/ExceptionChecker.java
+++
@@ -1,7 +0,0 @@
-package engineexceptions;
-
-import java.io.Serializable;
-
-public class ExceptionChecker extends Implementer implements Serializable{
-	private static final long serialVersionUID = 36569891;
-}
