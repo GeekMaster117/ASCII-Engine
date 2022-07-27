f1ad25892c271c423dd3adfbdfadf8d1974f8915
GeekMaster117 <saivenkat0306@gmail.com>
2022-07-27 21:16:28 +0530

    Renaming

--- engineExceptions/ExceptionChecker.java
+++
@@ -1,7 +0,0 @@
-package engineExceptions;
-
-import java.io.Serializable;
-
-public class ExceptionChecker extends Implementer implements Serializable{
-	private static final long serialVersionUID = 36569891;
-}
