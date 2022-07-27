341bb685f5e2b8295fb615706ee7fd76cfb2c2e9
GeekMaster117 <saivenkat0306@gmail.com>
2022-07-27 20:37:53 +0530

    Code

--- src/engineexceptions/checkers/ScreenException.java
+++
@@ -1,9 +0,0 @@
-package engineexceptions.checkers;
-
-import java.nio.file.Path;
-
-import engineexceptions.exceptions.IllegalFileException;
-
-public interface ScreenException {
-	public void CheckSameClassException(Path path) throws IllegalFileException;
-}
