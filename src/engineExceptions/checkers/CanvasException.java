1afe9191dd27ebd4ca3064873b1f789ebedc31ea
GeekMaster117 <saivenkat0306@gmail.com>
2022-07-27 21:06:51 +0530

    Code

--- src/engineExceptions/checkers/CanvasException.java
+++
@@ -1,8 +0,0 @@
-package engineExceptions.checkers;
-
-import engineExceptions.exceptions.OutOfBoundException;
-
-public interface CanvasException {
-	public void CheckCanvasDimensionException(int width,int height) throws OutOfBoundException;
-	public void CheckCanvasBoundException(int x,int y,int width,int height) throws OutOfBoundException;
-}
--- src/engineExceptions/checkers/ScreenException.java
+++
@@ -1,9 +0,0 @@
-package engineExceptions.checkers;
-
-import java.nio.file.Path;
-
-import engineExceptions.exceptions.IllegalFileException;
-
-public interface ScreenException {
-	public void CheckSameClassException(Path path) throws IllegalFileException;
-}
